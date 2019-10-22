4f6fe4ecffb9efdc290e91d47bc03c3296e68b5a
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-10-22 11:10:46 -0400

    Complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,36 @@ public class Music {
 
     private String[] playList;
 
+
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int i;
+        int j;
+        int counterA = 0;
+        int counterB = 0;
+      for( i = 0; i <= startIndex; i++){
+          counterA++;
+      }
+
+      for(j = i; j <= playList.length; j++){
+          counterB++;
+      }
+
+        if(counterA < counterB){
+            return counterA;
+        } else {
+            return counterB;
+        }
+      }
+
+
     }
-}
+
+
+//down button method
+//up button method
+//counter for minimum number of button presses
