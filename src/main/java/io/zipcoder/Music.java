ff79beb439e785605c2f1dce585758bc282ff5cd
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-10-25 11:09:13 -0400

    Updated with smarter code

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,30 +8,51 @@ public class Music {
     public Music(String[] playList){
 
         this.playList = playList;
-    }
+   }
+//
+//    public Integer selection(Integer startIndex, String selection){
+//        int i;
+//        int j;
+//        int counterA = 0;
+//        int counterB = 0;
+//      for( i = 0; i <= startIndex; i++){
+//          counterA++;
+//      }
+//
+//      for(j = i; j <= playList.length; j++){
+//          counterB++;
+//      }
+//
+//        if(counterA < counterB){
+//            return counterA;
+//        } else {
+//            return counterB;
+//        }
+//      }
 
-    public Integer selection(Integer startIndex, String selection){
-        int i;
-        int j;
-        int counterA = 0;
-        int counterB = 0;
-      for( i = 0; i <= startIndex; i++){
-          counterA++;
-      }
+    public Integer selection(Integer startIndex, String selection) {
+        int forwardsCounter = 0;
+        int backwardsCounter = 0;
 
-      for(j = i; j <= playList.length; j++){
-          counterB++;
-      }
-
-        if(counterA < counterB){
-            return counterA;
-        } else {
-            return counterB;
+        for (int i = 0; i < startIndex + 1; i++) {
+            forwardsCounter++;
+            if (playList[i].equals(selection)) {
+                break;
+            }
         }
-      }
-
-
+        for (int j = startIndex; j < playList.length; j++){
+            backwardsCounter ++;
+            if(playList[j].equals(selection)){
+                break;
+            }
+        }
+        if (forwardsCounter > backwardsCounter){
+            return backwardsCounter;
+        } else {
+            return forwardsCounter;}
     }
+}
+
 
 
 //down button method
